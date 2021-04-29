f2e5d65f620eb92c2dc490a0fdb46338072524e2
Changyu Gao <ustcgcy@gmail.com>
2021-04-29 14:04:58 -0500

    Fix Thompson

--- BayesOpt.py
+++ BayesOpt.py
@@ -7,8 +7,11 @@ from torch.distributions import constraints, transform_to
 
 import pyro
 import pyro.contrib.gp as gp
+import pyro.distributions as dist
 
-Z_dist = pyro.distributions.Normal(0, 1)
+import numpy as np
+
+Z_dist = dist.Normal(0, 1)
 
 
 class GPBO(object):
@@ -21,12 +24,14 @@ class GPBO(object):
         kernel=gp.kernels.Matern52,
         acquistion_fn="EI",
         num_candidates=5,
+        grid_resolution=10,
     ):
         # X = toTorch(X)
         # y = toTorch(y)
         if len(X.shape) == 1:
             X = X.unsqueeze(1)
         self.input_dim = X.shape[1]
+        self.next_x_fn = self.next_x
         if acquistion_fn == "EI":
             self.acquistion_fn = self.expected_improvement
         elif acquistion_fn == "LCB":
@@ -34,7 +39,9 @@ class GPBO(object):
         elif acquistion_fn == "PI":
             self.acquistion_fn = self.probability_improvement
         elif acquistion_fn == "Thompson":
-            self.acquistion_fn = self.thompson
+            self.next_x_fn = self.next_x_thompson
+        elif acquistion_fn == "random":
+            self.next_x_fn = self.next_x_random
         self.f = f
         self.lower_bound, self.upper_bound = bound
         self.num_candidates = num_candidates
@@ -51,11 +58,23 @@ class GPBO(object):
         self.x_hist = [self.x_min]
         self.y_hist = [self.y_min]
 
-    def update_posterior(self, x_new):
+        zz = [
+            np.linspace(
+                self.lower_bound, self.upper_bound, grid_resolution, endpoint=False
+            )
+        ] * self.input_dim
+        # equivalent to the Cartesian product(*zz)
+        self.grid = torch.Tensor(
+            np.array(np.meshgrid(*zz)).T.reshape(-1, self.input_dim)
+        )
+        self.grid_delta = (self.upper_bound - self.lower_bound) / grid_resolution
+
+    def update_posterior(self, x_new, log):
         y_new = self.f(x_new)  # evaluate f at new point.
         # if len(torch.tensor(y_new).shape) == 2:
         y_new = y_new.squeeze(1)
-        print(f"x={x_new[0].numpy()}, f={y_new.item():.4f}")
+        if log:
+            print(f"x={x_new[0].numpy()}, f={y_new.item():.4f}")
         if y_new.item() < self.y_min:
             self.x_min, self.y_min = x_new[0].numpy(), y_new.item()
         self.x_hist.append(self.x_min)
@@ -66,10 +85,6 @@ class GPBO(object):
         # optimize the GP hyperparameters using Adam with lr=0.001
         optimizer = torch.optim.Adam(self.gpmodel.parameters(), lr=0.001)
         gp.util.train(self.gpmodel, optimizer)
-
-    def thompson(self, x):
-        mu, variance = self.gpmodel(x, full_cov=False, noiseless=False)
-        return mu + variance.sqrt() * pyro.distributions.Normal(0, 1).rsample()
 
     def lower_confidence_bound(self, x, kappa=2):
         mu, variance = self.gpmodel(x, full_cov=False, noiseless=False)
@@ -90,9 +105,9 @@ class GPBO(object):
             (self.y_min - xi - mu) * Z_dist.cdf(Z) + sigma * Z_dist.log_prob(Z).exp()
         )
 
-    def find_a_candidate(self, x_init, lower_bound=0, upper_bound=1):
+    def find_a_candidate(self, x_init):
         # transform x to an unconstrained domain
-        constraint = constraints.interval(lower_bound, upper_bound)
+        constraint = constraints.interval(self.lower_bound, self.upper_bound)
         unconstrained_x_init = transform_to(constraint).inv(x_init)
         unconstrained_x = unconstrained_x_init.clone().detach().requires_grad_(True)
         minimizer = optim.LBFGS([unconstrained_x], line_search_fn="strong_wolfe")
@@ -111,19 +126,35 @@ class GPBO(object):
         x = transform_to(constraint)(unconstrained_x)
         return x.detach()
 
-    def next_x(self, lower_bound=0, upper_bound=1, num_candidates=5):
+    def next_x(self):
         candidates = []
         values = []
 
         x_init = self.gpmodel.X[-1:]
-        for i in range(num_candidates):
-            x = self.find_a_candidate(x_init, lower_bound, upper_bound)
+        for i in range(self.num_candidates):
+            x = self.find_a_candidate(x_init)
             y = self.acquistion_fn(x)
             candidates.append(x)
             values.append(y)
-            x_init = x.new_empty((1, self.input_dim)).uniform_(lower_bound, upper_bound)
+            x_init = x.new_empty((1, self.input_dim)).uniform_(
+                self.lower_bound, self.upper_bound
+            )
         argmin = torch.min(torch.cat(values), dim=0)[1].item()
         return candidates[argmin]
+
+    def next_x_thompson(self):
+        # print(len(self.grid))
+        pts = self.grid + self.grid_delta * torch.rand_like(self.grid)
+        mean, cov = self.gpmodel(pts, full_cov=True, noiseless=False)
+        cov += (cov.diag().max() * self.gpmodel.noise) * torch.eye(len(self.grid))
+        sample = dist.MultivariateNormal(mean, cov).sample()
+        min_idx = torch.argmin(sample)
+        return pts[[min_idx.item()]]
+
+    def next_x_random(self):
+        return dist.Uniform(self.lower_bound, self.upper_bound).sample(
+            (1, self.input_dim)
+        )
 
     def plot(self, gs, xmin, xlabel=None, with_title=True):
         xlabel = "xmin" if xlabel is None else "x{}".format(xlabel)
@@ -168,18 +199,20 @@ class GPBO(object):
             ax2.set_ylabel("Acquisition Function")
         ax2.legend(loc=1)
 
-    def train(self, iter=8):
+    def train(self, iter=8, log=False):
         plt.figure(figsize=(12, 30))
-        outer_gs = gridspec.GridSpec(5, 2)
+        if self.input_dim == 1:
+            outer_gs = gridspec.GridSpec(5, 2)
         optimizer = torch.optim.Adam(self.gpmodel.parameters(), lr=0.001)
         gp.util.train(self.gpmodel, optimizer)
         for i in range(iter):
-            xmin = self.next_x(self.lower_bound, self.upper_bound, self.num_candidates)
+            xmin = self.next_x_fn()
             if self.input_dim == 1 and i < 10:
                 gs = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=outer_gs[i])
                 self.plot(gs, xmin, xlabel=i + 1, with_title=(i % 2 == 0))
-            print(f"iter {i}: ", end='')
-            self.update_posterior(xmin)
+            if log and i % 10 == 0:
+                print(f"iter {i}: ", end="")
+                self.update_posterior(xmin, log)
         print(f"Final result: {self.x_min}, {self.y_min:.4f}")
         plt.figure()
         plt.plot(self.y_hist)
